1555ee2094e9e812af9310a5b1c423649115ab05
Beatrixirene <beatrixirene@gmail.com>
2022-03-01 08:28:03 +0700

    f

--- src/abi/foxieabi.js
+++ src/abi/foxieabi.js
@@ -365,3 +365,152 @@ export const FOXIE_ABI = [
   ]
 export const FOXIE_ROPSTEN = '0x101A0caa90138BBb55500c982Ce7581830Aa815F'
 export const FOXIE_RINKEBY = '0x3c8e063Bf671a3E3aea8E625935075e6b3440557'
+//
+export const RINKEBY_URL="https://eth-rinkeby.alchemyapi.io/v2/d5jl96uz-JVZRnR4LtYWZVsibWP4wYaz"
+export const ROPSTEN_URL="https://eth-ropsten.alchemyapi.io/v2/a4V3Hu3w_K6Uvjfb8DVXpYfuYmbaFWHp"
+//
+export const CONTRACT_ABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      }
+    ],
+    "name": "Allowance",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "spender",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      },
+      {
+        "internalType": "contract IERC20",
+        "name": "_token",
+        "type": "address"
+      }
+    ],
+    "name": "deposit",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "contract IERC20",
+        "name": "_token",
+        "type": "address"
+      }
+    ],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      },
+      {
+        "internalType": "contract IERC20",
+        "name": "_token",
+        "type": "address"
+      }
+    ],
+    "name": "withDraw",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  }
+]
+export const CONTRACT_ADDRESS_ROPSTEN = '0x3ad0F43D472D6ADAcd330CF5c2030aCf3CbA3B1D'
+export const CONTRACT_ADDRESS_RINKEBY = '0xBc905723A04217C76A451C8aF10011b244f3bfE8'
--- src/components/crosschaintoken.js
+++ src/components/crosschaintoken.js
@@ -2,41 +2,82 @@ import './style/crosschaintoken.css'
 import { React, useEffect, useState } from 'react'
 // import ReactDOM from "react-dom";
 import {
-  Select,
-  Box,
-  MenuItem,
-  InputLabel,
-  FormControl,
-  Avatar,
-  Button,
-  CircularProgress,
-  TextField
+  Select, Box,
+  MenuItem, InputLabel,
+  FormControl, Avatar,
+  Button, CircularProgress, TextField,
 } from '@mui/material';
+import SwapVertIcon from '@mui/icons-material/SwapVert';
 import { useSnackbar } from 'notistack';
 import Web3 from 'web3';
 import axios from 'axios'
-import { FOXIE_ABI, FOXIE_ROPSTEN, FOXIE_RINKEBY } from '../abi/foxieabi'
+import {
+  FOXIE_ABI, FOXIE_ROPSTEN,
+  FOXIE_RINKEBY, RINKEBY_URL,
+  ROPSTEN_URL, CONTRACT_ADDRESS_ROPSTEN,
+  CONTRACT_ADDRESS_RINKEBY, CONTRACT_ABI,
+} from '../abi/foxieabi'
+import { createAlchemyWeb3 } from "@alch/alchemy-web3"
+import { Grow, Slide, Zoom } from '@mui/material';
+
 
 
 function Crosschaineth() {
   const [tokenAddress, setTokenAddress] = useState([]);
   const [arr, setArr] = useState([]);
   const [networkToken, setNetworkToken] = useState();
+  const [networkSwapToken, setNetworkSwapToken] = useState();
+  const [addressSwapToken, setAddressSwapToken] = useState();
   const [balanceTokenRopsten, setBalanceNetworkRopsten] = useState();
+  const [balanceTokenRinkeby, setBalanceTokenRinkeby] = useState();
+  const [amountUp, setAmountUp] = useState();
   const web3 = new Web3(Web3.givenProvider);
+  const { enqueueSnackbar } = useSnackbar();
+  const web3AlchemyRopsten = createAlchemyWeb3(ROPSTEN_URL)
+  const web3AlchemyRinkeby = createAlchemyWeb3(RINKEBY_URL)
 
-  const getBalance = async () => {
-    const CONTRACT_FOXIE_ROPSTEN = new web3.eth.Contract(FOXIE_ABI, FOXIE_ROPSTEN)
-    const account = await web3.eth.getAccounts()
+  const getBalanceRopsten = async () => {
+    const CONTRACT_FOXIE_ROPSTEN = new web3AlchemyRopsten.eth.Contract(FOXIE_ABI, FOXIE_ROPSTEN)
+    const account = await web3AlchemyRopsten.eth.getAccounts()
     const balance = await CONTRACT_FOXIE_ROPSTEN.methods.balanceOf(account[0]).call()
-    setBalanceNetworkRopsten(web3.utils.fromWei(balance, 'ether'))
+    setBalanceNetworkRopsten(web3AlchemyRopsten.utils.fromWei(balance, 'ether'))
+  }
+  const getBalanceRinkeby = async () => {
+    const CONTRACT_FOXIE_RINKEBY = new web3AlchemyRinkeby.eth.Contract(FOXIE_ABI, FOXIE_RINKEBY)
+    const account = await web3AlchemyRinkeby.eth.getAccounts()
+    const balance = await CONTRACT_FOXIE_RINKEBY.methods.balanceOf(account[0]).call()
+    setBalanceTokenRinkeby(web3AlchemyRinkeby.utils.fromWei(balance, 'ether'))
   }
 
   const handleChange = async (event) => {
     setTokenAddress(event.target.value);
     let addressToken = event.target.value;
-    await axios.get(`http://localhost:3001/tokens/params/${addressToken}`).then(data => setNetworkToken(data.data.network))
+    const result = await axios.get(`http://localhost:3001/tokens/params/${addressToken}`);
+    setNetworkToken(result.data.network);
+    (result.data.address === arr[0].address) ? setAddressSwapToken(arr[1].address) : setAddressSwapToken(arr[0].address);
+    (result.data.network === arr[0].network) ? setNetworkSwapToken(arr[1].network) : setNetworkSwapToken(arr[0].network);
   };
+  const handleAddress = async () => {
+    const copyAddress = document.getElementById("address-btn1");
+    await navigator.clipboard.writeText(copyAddress.value);
+    enqueueSnackbar('Address has been copied success!!!', {
+      anchorOrigin: {
+        vertical: 'top',
+        horizontal: 'center',
+      },
+      TransitionComponent: Slide,
+      variant: 'success',
+      sx: {
+        "& .SnackbarContent-root": {
+          bgcolor: '#0ed816',
+          borderRadius: '4px',
+          boxShadow: 'rgb(60 66 87 / 12%) 0px 7px 14px 0px, rgb(0 0 0 / 12%) 0px 3px 6px 0px',
+          color: '#fff',
+          fontSize: 18
+        }
+      }
+    });
+  }
 
   const getDataToken = async () => {
     const tokens = await axios({
@@ -48,11 +89,36 @@ function Crosschaineth() {
       setArr(datas => [...datas, data])
     })
   }
+  const onchangeValue = (e) => {
+    let amount = e.target.value
+    setAmountUp(amount)
+  }
+  const depositToken = async () => {
+    const CONTRACT_ROPSTEN = new web3AlchemyRopsten.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS_ROPSTEN)
+    const CONTRACT_RINKEBY = new web3AlchemyRinkeby.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS_RINKEBY)
+    const amountFix = web3.utils.toWei(amountUp, 'ether')
+    console.log(amountFix)
+    const account = await web3.eth.getAccounts()
+    switch (networkToken) {
+      case 'Ropsten Testnet Network':
+        await CONTRACT_ROPSTEN.methods.deposit(amountFix, FOXIE_ROPSTEN).send({
+          from: account[0]
+        });
+      // case 'Rinkeby Testnet Network':
+      //   return (await CONTRACT_RINKEBY.methods.deposit(amountFix, FOXIE_RINKEBY).call());
+      // default:
+      //   return console.log(undefined);
+    }
+  }
 
   useEffect(async () => {
     getDataToken()
-    getBalance()
   }, [])
+
+  useEffect(async () => {
+    getBalanceRinkeby()
+    getBalanceRopsten()
+  }, [addressSwapToken])
 
   return (
     <div className="App">
@@ -104,7 +170,62 @@ function Crosschaineth() {
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
                         value={tokenAddress ? tokenAddress : ""}
-                        label="Age"
+                        label="Token"
+                        onChange={handleChange}>
+                        {
+                          arr.map((data, index) => {
+                            return <MenuItem value={data.address} key={index}>{data.token}</MenuItem>
+                          })
+                        }
+                      </Select>
+                    </FormControl>
+                  </Box>
+
+                  {
+                    (tokenAddress.length > 0) ? <Button variant="text" id="address-btn1" onClick={handleAddress} value={tokenAddress}>{"Address Token: " + tokenAddress}</Button> : ""
+                  }
+                </div>
+                <span className="padding-bottom--15"></span>
+                <Box sx={{ maxWidth: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                  <Button disabled sx={{ color: '#5469d4' }}>{tokenAddress.length > 0 ? "Network Token: " + networkToken : "Network Token: "}</Button>
+                  <Button disabled>{tokenAddress.length > 0 ? "Balance:" + balanceTokenRopsten : ""}</Button>
+                </Box>
+                <span className="padding-bottom--15"></span>
+                <Box sx={{ maxWidth: "100%" }}>
+                  <TextField
+                    id="outlined-number"
+                    label="Amount"
+                    type="number"
+                    sx={{ width: '100%' }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    onChange={onchangeValue} />
+                </Box>
+                <span className="padding-bottom--15"></span>
+                <Box sx={{ maxWidth: "100%", display: 'flex', justifyContent: 'center' }}>
+                  <SwapVertIcon
+                    sx={{
+                      fontSize: 40,
+                      color: 'rgb(84, 105, 212)',
+                      cursor: 'pointer',
+                      transitions: "2s",
+                      '&:hover': {
+                        /////////
+                      }
+                    }}
+                  />
+                </Box>
+                <span className="padding-bottom--15"></span>
+                <div className="field" style={{ "display": "flex", "flexDirection": "row" }}>
+                  <Box sx={{ maxWidth: "50%" }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">Token</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={tokenAddress ? tokenAddress : ""}
+                        label="Network"
                         onChange={handleChange}>
                         {
                           arr.map((data, index) => {
@@ -115,12 +236,31 @@ function Crosschaineth() {
                     </FormControl>
                   </Box>
                   {
-                    (tokenAddress.length > 0) ? <Button variant="text">{"Address Token: " + tokenAddress}</Button> : ""
+                    (tokenAddress.length > 0) ? <Button variant="text" id="address-btn2" value={addressSwapToken}>{"Address Token: " + addressSwapToken}</Button> : ""
                   }
                 </div>
+                <span className="padding-bottom--15"></span>
                 <Box sx={{ maxWidth: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                  <Button disabled>{tokenAddress.length > 0? "Network Token: " + networkToken: "Network Token: "}</Button>
-                  <Button disabled>{tokenAddress.length > 0? "Balance:" + balanceTokenRopsten: ""}</Button>
+                  <Button disabled sx={{ color: '#5469d4' }}>{tokenAddress.length > 0 ? "Network Token: " + networkSwapToken : "Network Token: "}</Button>
+                  <Button disabled>{tokenAddress.length > 0 ? "Balance:" + balanceTokenRinkeby : ""}</Button>
+                </Box>
+                <span className="padding-bottom--15"></span>
+                <Box sx={{ maxWidth: "100%" }}>
+                  <TextField
+                    id="outlined-number"
+                    label="Amount"
+                    type="number"
+                    sx={{ width: '100%' }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }} />
+                </Box>
+                <span className="padding-bottom--15"></span>
+                <span className="padding-bottom--15"></span>
+                <Box sx={{ maxWidth: "100%", display: 'flex', justifyContent: 'center' }}>
+                  <Button variant="contained" disableElevation sx={{ background: '#5469d4', padding: 2 }} onClick={depositToken}>
+                    CLICK HERE TO SWAP
+                  </Button>
                 </Box>
               </div>
             </div>
